98b287b9e740a309a55bd701697b717d58cd463f
Isaac David <isaac9810@hotmail.com>
2023-02-20 11:32:09 -0600

    livelock

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,47 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-       // Create participants and resources
-                Fox robin = new Fox();
-                Fox miki = new Fox();
-                Food food = new Food();
-                Water water = new Water();
-        // Process data
-                ExecutorService service = null;
-                try {
-                    service = Executors.newScheduledThreadPool(10);
-                    service.submit(() -> robin.eatAndDrink(food,water));
-                    service.submit(() -> miki.drinkAndEat(food,water));
-                } finally {
-                    if(service != null) service.shutdown();
-                }
+    // Create participants and resources
+            Fox robin = new Fox();
+            Fox miki = new Fox();
+            Food food = new Food();
+            Water water = new Water();
+    // Process data
+            ExecutorService service = null;
+            try {
+                service = Executors.newScheduledThreadPool(10);
+                service.submit(() -> robin.eatAndDrink(food,water));
+                service.submit(() -> miki.drinkAndEat(food,water));
+            } finally {
+                if(service != null) service.shutdown();
             }
         }
+    }
 
-        class Food {}
+    class Food {}
 
-        class Water {}
+    class Water {}
 
-        class Fox {
-            public void eatAndDrink(Food food, Water water) {
-                synchronized(food) {
-                    System.out.println("Robin: Got deadlock.Food!");
-                    move();
-                    synchronized(water) {
-                        System.out.println("Robin: Got deadlock.Water!");
-                    }
-                }
+    class Fox {
+        public void eatAndDrink(Food food, Water water) {
+            synchronized(food) {
+                System.out.println("Got Food!");
+                move();
             }
-            public void drinkAndEat(Food food, Water water) {
-                synchronized(water) {
-                    System.out.println("Miki: Got deadlock.Water!");
-                    move();
-                    synchronized(food) {
-                        System.out.println("Miki: Got deadlock.Food!");
-                    }
-                }
+            drinkAndEat(food, water);
+        }
+        public void drinkAndEat(Food food, Water water) {
+            synchronized(water) {
+                System.out.println("Got Water!");
+                move();
             }
-            public void move() {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-        // Handle exception
-                }
+            eatAndDrink(food, water);
+        }
+        public void move() {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+    // Handle exception
+            }
     }
 }
