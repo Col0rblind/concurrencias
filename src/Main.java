1b72754e93873c330b6e9c8c65d462245e543eac
Isaac David <isaac9810@hotmail.com>
2023-02-20 11:21:49 -0600

    deadlock

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,53 @@
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Counter counter =  new Counter();
-
-        Thread first = new Thread(counter, "First");
-        Thread second = new Thread(counter, "Second");
-
-        first.start();
-        second.start();
-
-        first.join();
-        second.join();
-
-        System.out.println(counter.count);
-
-    }
-    /* System.out.println("Start");
-        Thread.sleep(2000);
-        System.out.println("End");*/
-    static class Counter extends Thread{
-        public AtomicInteger count = new AtomicInteger(0);
-
-        public void run(){
-            for (int i = 0; i < 100_000_000; i++) {
-                count.addAndGet(1);
+    public static void main(String[] args) {
+       // Create participants and resources
+                Fox robin = new Fox();
+                Fox miki = new Fox();
+                Food food = new Food();
+                Water water = new Water();
+        // Process data
+                ExecutorService service = null;
+                try {
+                    service = Executors.newScheduledThreadPool(10);
+                    service.submit(() -> robin.eatAndDrink(food,water));
+                    service.submit(() -> miki.drinkAndEat(food,water));
+                } finally {
+                    if(service != null) service.shutdown();
+                }
             }
         }
-    }
 
+        class Food {}
+
+        class Water {}
+
+        class Fox {
+            public void eatAndDrink(Food food, Water water) {
+                synchronized(food) {
+                    System.out.println("Robin: Got deadlock.Food!");
+                    move();
+                    synchronized(water) {
+                        System.out.println("Robin: Got deadlock.Water!");
+                    }
+                }
+            }
+            public void drinkAndEat(Food food, Water water) {
+                synchronized(water) {
+                    System.out.println("Miki: Got deadlock.Water!");
+                    move();
+                    synchronized(food) {
+                        System.out.println("Miki: Got deadlock.Food!");
+                    }
+                }
+            }
+            public void move() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+        // Handle exception
+                }
+    }
 }
