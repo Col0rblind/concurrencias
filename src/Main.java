a3d6a912f12ecf574a28ae42acb081bca3ac4a59
Isaac David <isaac9810@hotmail.com>
2023-02-20 10:45:54 -0600

    webscraper

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,67 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        //Streams  / lambda
+    public static void main(String[] args) throws IOException {
+        //Download webs
+        List<String> links  = new ArrayList<>();
+        links.add("https://www.bbc.com/");
+        links.add("https://www.bbc.com/news/world-europe-64702548");
+        links.add("https://www.bbc.com/news/world-europe-64637091");
+        links.add("https://www.bbc.com/news/world-latin-america-64701062");
+        links.add("https://www.bbc.com/sport/football/64708314");
+        links.add("https://www.bbc.com/sport/football/64704536");
+        links.add("https://www.bbc.com/sport/live/cricket/64045176");
+        links.add("https://www.bbc.com/news/live/world-europe-64702090");
 
-        List<String> cities = new ArrayList<>();
-        cities.add("London");
-        cities.add("New York");
-        cities.add("Tokyo");
-        cities.add("Barcelona");
-        cities.add("Buenos Aires");
-        cities.add("Bogotá");
-        cities.add("Ciudad de México");
-        cities.add("Lima");
+        /* Long timeStart = System.nanoTime();
+        Esto para ver cuanto tiempo tarda en cargar y ejecutar un proceso o función. Se debe omitier 9 decimales para ver el tiempo en segundos
+        links.stream().forEach(link -> getWebContent(link)); Demora 2708304600
+        Long timeEnd = System.nanoTime();
+        System.out.println("Difference: " + (timeEnd -timeStart));
 
-        //índice para recorrer
-         /* for (int i  = 0; i < cities.size(); i++){
-            System.out.println(cities.get(i));
-        }
-        Sin índice para recorrer
-         For each
-            for (String city : cities){
-            System.out.println(city);
-            }
+        timeStart = System.nanoTime();
+        links.stream().parallel().forEach(link -> getWebContent(link)); Demora 1430529700
+        timeEnd = System.nanoTime();
+        System.out.println("Difference: " + (timeEnd -timeStart));
+
         */
 
-        //Nueva forma de recorrer arrays en Lambda
-
-        cities.stream().forEach(city -> System.out.println(city));
-
-        //Reference to a method
-        cities.stream().forEach(Main::printCity);
-
-        //simple example of parralel - pipeline
-        cities.stream().parallel().forEach(System.out::println);
-
-        //filter
-        cities.stream().filter(city -> city.startsWith("B")).forEach(System.out::println);
-
-        //Filtros con list y method satic tolist
-        List<String> filteredCities = cities.stream().filter(city -> city.startsWith("B"))
-                .filter(city -> city.contains("n"))
-                .collect(Collectors.toList());
+        links.stream().parallel().forEach(link -> getWebContent(link));
+        String link  = "https://www.bbc.com/";
+        String result = getWebContent(link);
 
     }
-        public static boolean filterCity(String city){
-            return city.startsWith("B");
+
+    public synchronized static String getWebContent(String link) {
+            System.out.println("INIT");
+            System.out.println(link);
+        try {
+            URL url = new URL(link);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            String encoding = conn.getContentEncoding();
+
+            InputStream input = conn.getInputStream();
+
+            Stream <String> lines = new BufferedReader(new InputStreamReader(input))
+                    .lines();
+
+            System.out.println("END");
+
+            return lines.collect(Collectors.joining());
+
+        } catch (IOException e){
+            System.out.print(e.getMessage());
         }
-        public static void printCity(String city){
-            System.out.println(city);
-        }
+        return "";
+    }
 }
